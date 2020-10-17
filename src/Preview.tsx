2b005c42aa1c65506f6f59c84a0f2cdcd6df3afc
Shi Han NG <shihanng@gmail.com>
2020-10-17 11:35:16 +0900

    Use react-grid-heatmap

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import { useQueryParam, NumberParam, DateParam } from "use-query-params";
+import { HeatMapGrid } from "react-grid-heatmap";
+
+const xLabels = new Array(24).fill(0).map((_, i) => `${i}`);
+const yLabels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+const data = new Array(yLabels.length)
+  .fill(0)
+  .map(() =>
+    new Array(xLabels.length)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * 50 + 50))
+  );
 
 const Preview = () => {
   const [years] = useQueryParam("y", NumberParam);
@@ -9,6 +20,34 @@ const Preview = () => {
     <div>
       <h1>Years {years}</h1>
       <h1>From {fromDate?.toString()}</h1>
+      <HeatMapGrid
+        data={data}
+        xLabels={xLabels}
+        yLabels={yLabels}
+        // Reder cell with tooltip
+        cellRender={(x, y, value) => (
+          <div title={`Pos(${x}, ${y}) = ${value}`}>{value}</div>
+        )}
+        xLabelsStyle={(index) => ({
+          color: index % 2 ? "transparent" : "#777",
+          fontSize: ".8rem",
+        })}
+        yLabelsStyle={() => ({
+          fontSize: ".7rem",
+          textTransform: "uppercase",
+          color: "#777",
+        })}
+        cellStyle={(_x, _y, ratio) => ({
+          background: `rgb(12, 160, 44, ${ratio})`,
+          fontSize: ".8rem",
+          color: `rgb(0, 0, 0, ${ratio / 2 + 0.4})`,
+        })}
+        cellHeight="2rem"
+        xLabelsPos="bottom"
+        onClick={(x, y) => alert(`Clicked (${x}, ${y})`)}
+        yLabelsPos="right"
+        square
+      />
     </div>
   );
 };
