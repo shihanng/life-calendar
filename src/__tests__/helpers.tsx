3aa54f6cf4a5c410a74469a5ef9f4e0cf855a6ce
Shi Han NG <shihanng@gmail.com>
2020-10-25 20:34:15 +0900

    Add generateData

--- src/__tests__/helpers.tsx
+++ src/__tests__/helpers.tsx
@@ -1,4 +1,5 @@
-import { generateDataArray } from "../helpers";
+import { generateDataArray, generateData } from "../helpers";
+import dayjs from "dayjs";
 
 test("boundaries and bins are same", () => {
   const data = generateDataArray(1, 10, 10, 3, 8);
@@ -13,4 +14,17 @@ test("5 bins", () => {
 test("start and end are also boundaries", () => {
   const data = generateDataArray(1, 10, 10, 1, 10);
   expect(data).toEqual([100, 100, 100, 100, 100, 100, 100, 100, 100, 100]);
+});
+
+test("test generateData", () => {
+  const data = generateData(dayjs("2020-12-25"), dayjs("2020-12-25"), 1);
+  expect(data).toHaveLength(53);
+  expect(data[0]).toEqual({
+    startDate: dayjs("2020-12-20"),
+    days: 1,
+  });
+  expect(data[1]).toEqual({
+    startDate: dayjs("2020-12-27"),
+    days: 0,
+  });
 });
--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,3 +1,12 @@
+import dayjs, { Dayjs } from "dayjs";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import isBetween from "dayjs/plugin/isBetween";
+import weekday from "dayjs/plugin/weekday";
+
+dayjs.extend(isSameOrBefore);
+dayjs.extend(weekday);
+dayjs.extend(isBetween);
+
 export const generateDataArray = (
   lowerBound: number,
   upperBound: number,
@@ -28,4 +37,34 @@ export const generateDataArray = (
     endVal ? endVal : 100,
     ...rightData,
   ];
+};
+
+interface Week {
+  startDate: Dayjs;
+  days: number;
+}
+
+export const generateData = (
+  startDate: Dayjs,
+  todayDate: Dayjs,
+  years: number
+): Array<Week> => {
+  const endDate = startDate.add(years, "year");
+
+  let currentDate = startDate.weekday(0);
+  let data: Array<Week> = [];
+
+  while (currentDate.isSameOrBefore(endDate)) {
+    if (currentDate.day() == 0) {
+      data.push({ startDate: currentDate, days: 0 });
+    }
+
+    if (currentDate.isBetween(startDate, todayDate, null, "[]")) {
+      data[data.length - 1].days++;
+    }
+
+    currentDate = currentDate.add(1, "day");
+  }
+
+  return data;
 };
