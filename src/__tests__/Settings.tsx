e7f98d295fc4c6741c549330f1dfe69637b6abe6
Shi Han NG <shihanng@gmail.com>
2020-11-16 22:11:53 +0900

    Add test on years input

--- src/__tests__/Settings.tsx
+++ src/__tests__/Settings.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Settings from "../Settings";
 import { QueryParamProvider } from "use-query-params";
 
-test("renders learn react link", () => {
+test("change in years input should set query parameter", () => {
   const { getByLabelText } = render(
     <QueryParamProvider>
       <Settings />
     </QueryParamProvider>
   );
-  const input = getByLabelText("years");
+  const input = getByLabelText(/years/i);
+
   expect(input).toHaveAttribute("type", "number");
+  fireEvent.change(input, { target: { value: "10" } });
+  expect(window.location.search).toEqual("?y=10");
 });
