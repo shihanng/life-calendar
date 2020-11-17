aa05bf3f0ca1a100f1ff24a50885dedb0bd60cb4
Shi Han NG <shihanng@gmail.com>
2020-11-17 20:17:35 +0900

    Set from in Settings tests

--- src/__tests__/Settings.tsx
+++ src/__tests__/Settings.tsx
@@ -3,15 +3,20 @@ import { render, fireEvent } from "@testing-library/react";
 import Settings from "../Settings";
 import { QueryParamProvider } from "use-query-params";
 
-test("change in years input should set query parameter", () => {
+test("changes in inputs should set query parameter", () => {
   const { getByLabelText } = render(
     <QueryParamProvider>
       <Settings />
     </QueryParamProvider>
   );
-  const input = getByLabelText(/years/i);
 
-  expect(input).toHaveAttribute("type", "number");
-  fireEvent.change(input, { target: { value: "10" } });
-  expect(window.location.search).toEqual("?y=10");
+  const yearInput = getByLabelText(/years/i);
+  expect(yearInput).toHaveAttribute("type", "number");
+  fireEvent.change(yearInput, { target: { value: "10" } });
+
+  const fromInput = getByLabelText(/from/i);
+  expect(fromInput).toHaveAttribute("type", "date");
+  fireEvent.change(fromInput, { target: { value: "2020-11-20" } });
+
+  expect(window.location.search).toEqual("?d=2020-11-20&y=10");
 });
