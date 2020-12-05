e67a976abc58cf12ac5d5209d6d74778dbcebc90
Shi Han NG <shihanng@gmail.com>
2020-12-05 23:50:12 +0900

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import DayjsUtils from "@date-io/dayjs";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { RouteComponentProps } from "@reach/router";
 import React, { useEffect, useState } from "react";
 import { StringParam, useQueryParam } from "use-query-params";
 import Preview from "./Preview";
 import Settings from "./Settings";
+import dayjs from "dayjs";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: "auto",
+    backgroundColor: theme.palette.grey[200],
+  },
+  icon: {
+    color: theme.palette.text.primary,
+  },
+}));
 
 function App(_: RouteComponentProps) {
   const [qy, setQy] = useQueryParam("y", StringParam);
@@ -25,8 +47,10 @@ function App(_: RouteComponentProps) {
     setFromDate(fromDate);
   };
 
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <MuiPickersUtilsProvider utils={DayjsUtils}>
         <Container maxWidth="md">
@@ -41,6 +65,31 @@ function App(_: RouteComponentProps) {
           />
         </Container>
       </MuiPickersUtilsProvider>
+      <footer className={classes.footer}>
+        <Grid
+          spacing={3}
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item>
+            <Typography variant="body2">
+              &copy; Copyright {`${dayjs().format("YYYY")}`}, Shi Han NG
+            </Typography>
+          </Grid>
+          <Grid item>
+            <a href="https://twitter.com/shihanng">
+              <TwitterIcon className={classes.icon} />
+            </a>
+          </Grid>
+          <Grid item>
+            <a href="https://github.com/shihanng/life-count-down">
+              <GitHubIcon className={classes.icon} />
+            </a>
+          </Grid>
+        </Grid>
+      </footer>
     </div>
   );
 }
